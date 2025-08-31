274dce181a09a9e0ab2e37c062c954d35a3c2b3b
sikhareddy <seven.sikha@gmail.com>
2025-08-31 23:00:33 +0530

    initial changes

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Investment App Frontend</h1>
+      <p>Welcome to your React/TypeScript UI!</p>
+    </div>
+  );
+}
+
+export default App;
